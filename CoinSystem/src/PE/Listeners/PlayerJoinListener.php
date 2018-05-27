ce5c00edf562691140487c96706534b6bbef6bd6
zBearchenPlayz <38949581+zBearchenPlayz@users.noreply.github.com>
2018-05-27 19:35:23 +0200

    Create PlayerJoinListener.php

---
+++ CoinSystem/src/PE/Listeners/PlayerJoinListener.php
@@ -0,0 +1,15 @@
+<?php
+namespace zBearchenPlayz\Listener;
+use PE\coinsystem;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\event\player\PlayerLoginEvent;
+
+class PlayerJoinListener implements Listener{
+    
+    public function onJoin(PlayerJoinEvent){
+        
+    }
+}
