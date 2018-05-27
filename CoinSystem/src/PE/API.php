a668ad6bd838a931e5ac285e46af935a91d41176
zBearchenPlayz <38949581+zBearchenPlayz@users.noreply.github.com>
2018-05-27 19:33:17 +0200

    Create API.php

---
+++ CoinSystem/src/PE/API.php
@@ -0,0 +1,29 @@
+<?php 
+namespace PE;
+use PE\coinsystem;
+
+use pocketmine\event\Listeners;
+use pocketmine\Player;
+
+class API implements Listeners {
+    
+    public static function addPlayer(string $name){
+        
+    }
+    
+    public static function getCoins(string $player): int{
+        
+    }
+    
+    public static function setCoins(string $player, int $coins){
+        
+    }
+    
+    public static function addCoins(string $player, int $coins){
+        
+    }
+    
+    public static function removeCoins(string $player, int $coins){
+        
+    }    
+}
